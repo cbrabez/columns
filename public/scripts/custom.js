cf6f47d366410b4254bc0175a5a7802732a350d6
cbrabez <christian.brabez@gmail.com>
2019-01-31 14:45:20 +0000

    Add custom.js

---
+++ public/scripts/custom.js
@@ -0,0 +1,29 @@
+$(function(){
+   // delete tasks on checkbox change
+   $(".checkbox").on('change', function(){
+         var taskId = $(this).parent().attr('data-id');
+         $.ajax({
+            method: "POST",
+            url: "/tasks/" + taskId + "/?_method=DELETE",
+            data: {id: taskId},
+            success: function(result) {
+                console.log("SUCCESS DELETE");
+                location.reload(true);
+            }
+         });
+   });
+   
+   // delete task on button click
+   $(".delete-task").on('click', function(){
+      var taskId = $(this).parent().attr('data-id');
+      $.ajax({
+         method: "POST",
+         url: "/tasks/" + taskId + "/?_method=DELETE",
+         data: {id: taskId},
+         success: function(result) {
+             console.log("SUCCESS DELETE");
+             location.reload(true);
+         }
+      });
+   });
+});
